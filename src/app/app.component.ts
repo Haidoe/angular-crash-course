db8a06b2138ee89dd5e5275049a7338a72a79da0
haidoe <haidren.amalia@gmail.com>
2024-02-27 16:05:35 -0800

    feat: conditionals

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,20 +1,19 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { WishItem } from '../shared/models/wishItem';
-import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
+  imports: [RouterOutlet],
 })
 export class AppComponent {
-  items = [
-    new WishItem('To Learn Angular'),
-    new WishItem('Get Coffee', true),
-    new WishItem('Walk the doug'),
+  items: WishItem[] = [
+    // new WishItem('To Learn Angular'),
+    // new WishItem('Get Coffee', true),
+    // new WishItem('Walk the doug'),
   ];
 
   title = 'wishlist';
